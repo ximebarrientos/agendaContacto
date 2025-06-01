db17d861eba2f1dd1937ab9edb66d69c27af88fe
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-06-01 12:24:35 -0300

    creamos clase Contacto en nuevo archivo .js y lo exportamos a app.js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,35 @@
 import Contacto from "./classContacto.js";
 
 // el usuario cliquea el boton agregar invocar a una funcion que muestre el modal
-function agregarContacto(){
-   const modalCrearContacto = new bootstrap.Modal(document.getElementById('crearContacto'));
-    //mostrar ventana modal show es un metodo
-    modalCrearContacto.show();
-
+function abrirModalContacto() {
+  const modalCrearContacto = new bootstrap.Modal(
+    document.getElementById("crearContacto")
+  );
+  //mostrar ventana modal show es un metodo
+  modalCrearContacto.show();
 }
-//el usuario completa el form y debo crear un objeto contacto
+function crearContacto() {
+  //to do traer todos los datos del formulario validados
+  //crear un objeto Contacto
+  const nuevoContacto = new Contacto(
+    "juan",
+    "perez",
+    "23423423",
+    "juanperez@mail.com"
+  );
+  console.log(nuevoContacto);
+  //almacenar el objeto en la agenda
+}
 
 //declaro variables
-const btnAgregarContacto = document.getElementById('btnAgregarContacto');
-const formularioCrearContacto = document.querySelector('form');
+const btnAgregarContacto = document.getElementById("btnAgregarContacto");
+const formularioCrearContacto = document.querySelector("form");
+const agenda = [];
 
 //manejadores de eventos
-btnAgregarContacto.addEventListener('click', agregarContacto)
-formularioCrearContacto.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    console.log('aqui deberia crear un objeto contacto')
-    //crear un objeto Contacto
-    const nuevoContacto = new Contacto('juan', 'perez','23423423','juanperez@mail.com')
-    console.log(nuevoContacto)
-})
+btnAgregarContacto.addEventListener("click", abrirModalContacto);
+formularioCrearContacto.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //el usuario completa el form y debo crear un objeto contacto
+  crearContacto();
+});
--- js/classContacto.js
+++ js/classContacto.js
@@ -3,13 +3,17 @@ export default class Contacto{
     #nombre;
     #apellido;
     #telefono;
-    #email
-    constructor(nombre, apellido, telefono, email){
+    #email;
+    #imagen;
+    #notas;
+    constructor(nombre, apellido, telefono, email, imagen, notas){
         this.#id = crypto.randomUUID();//necesito un identificador unico
         this.#nombre = nombre;
         this.#apellido = apellido;
         this.#telefono = telefono;
         this.#email= email
+        this.#imagen=imagen
+        this.#notas=notas
     }
 
     //agregar los get y set
