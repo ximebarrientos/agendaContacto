471f6f1d3c5a005595203027d9c000573c544e23
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-06-16 12:40:42 -0300

    enviamos el id del contacto a la pagina de detalle

--- js/app.js
+++ js/app.js
@@ -69,7 +69,7 @@ function dibujarFila(contacto, index) {
                 <button class="btn btn-danger" onclick="eliminarContacto('${contacto.id}')">
                   <i class="bi bi-trash"></i>
                 </button>
-                <button class="btn btn-info"><i class="bi bi-eye"></i></button>
+                <button class="btn btn-info" onclick="verContacto('${contacto.id}')"><i class="bi bi-eye"></i></button>
               </td>
             </tr>`;
 }
@@ -151,6 +151,13 @@ window.prepararContacto = (id) => {
   //guardar el id del contacto que quiero modificar
   idContacto=id
 };
+
+window.verContacto=(id)=>{
+  console.log(id);
+  console.log(window.location)
+  window.location.href="/pages7detalleContacto.html?id="+id
+
+}
 
 //declaro variables
 const btnAgregarContacto = document.getElementById("btnAgregarContacto");
