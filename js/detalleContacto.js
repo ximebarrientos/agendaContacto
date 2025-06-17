77e45039945429473c9145932467a2159e3de34d
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-06-17 20:39:16 -0300

    mostramos el detalle contacto en la card horizontal

--- js/detalleContacto.js
+++ js/detalleContacto.js
@@ -1,12 +1,84 @@
 //1-buscar el parametro de la url
-console.log(window.location.search)
+console.log(window.location.search);
 
-const parametroURL=new URLSearchParams(window.location.search)
-const id=parametroURL.get("id")
-
+const parametroURL = new URLSearchParams(window.location.search);
+const id = parametroURL.get("id");
 
 //2-traer la agenda de contactos dle localstorage
+const agenda = JSON.parse(localStorage.getItem("agendaKey"));
 
 //-3 buscar en la agenda cuales son los datos del contacto que tiene el id recibido en la URL
+const contactoBuscado = agenda.find((contacto) => contacto.id === id);
 
 //4- dibujar el objeto en mi maquetado
+const cardContacto = document.querySelector(".card");
+
+cardContacto.innerHTML = `<div class="row g-0">
+            <div class="col-md-4">
+              <img
+                src="${contactoBuscado.imagen}"
+                class="img-fluid rounded-start"
+                alt="${contactoBuscado.nombre},${contactoBuscado.apellido}"
+              />
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <form>
+                  <div class="mb-3">
+                    <label for="nombre" class="form-label">Nombre</label>
+                    <input
+                      type="text"
+                      class="form-control bg-body-tertiary text-dark"
+                      id="nombre"
+                      aria-describedby="label nombre"
+                      minlength="2"
+                      maxlength="50"
+                      placeholder="Juan"
+                      value="${contactoBuscado.nombre}"
+                      disabled
+                    />
+                  </div>
+                  <div class="mb-3">
+                    <label for="apellido" class="form-label">Apellido</label>
+                    <input
+                      type="text"
+                      class="form-control bg-body-tertiary text-dark"
+                      id="apellido"
+                      minlength="2"
+                      maxlength="50"
+                      placeholder="Perez"
+                      value="${contactoBuscado.apellido}"
+                      disabled
+                    />
+                  </div>
+                  <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input
+                      type="email"
+                      class="form-control bg-body-tertiary text-dark"
+                      id="email"
+                      placeholder="juanperez@email.com"
+                      value="${contactoBuscado.email}"
+                      disabled
+                    />
+                  </div>
+                  <div class="mb-3">
+                    <label for="telefono" class="form-label">Telefono</label>
+                    <input
+                      type="tel"
+                      class="form-control bg-body-tertiary text-dark"
+                      id="telefono"
+                      placeholder="ej: +5493081225566"
+                      value="${contactoBuscado.telefono}"
+                      disabled
+                    />
+                  </div>
+
+                  <div class="mb-3">
+                    <label for="notas" class="form-label">Notas</label>
+                    <p>${contactoBuscado.notas}</p>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>`;
